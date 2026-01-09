a44e92570474a5f4c4bad5939b6bd8cec30f388a
CHANDANA G <234g1a0527@srit.ac.in>
2026-01-09 01:17:55 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-6097094105-2e5c2",
+  "appId": "1:23689693990:web:2356d80b90c835abfe4fe7",
+  "apiKey": "AIzaSyBCIpJbBBMSLDSPE7QihEoS-hmqOSO-rxs",
+  "authDomain": "studio-6097094105-2e5c2.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "23689693990"
+};
