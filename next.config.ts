d43630edb2707974a0cf6a1a7cf119615b49c144
CHANDANA G <234g1a0527@srit.ac.in>
2026-01-10 15:27:52 +0000

    https://www.collegebatch.com/static/clg-gallery/srinivasa-ramanujan-inst

--- next.config.ts
+++ next.config.ts
@@ -64,6 +64,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'www.collegebatch.com',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 };
