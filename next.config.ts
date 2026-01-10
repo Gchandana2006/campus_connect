30ac5ebff905abdf8798ec3c2ec29206e2cd540d
CHANDANA G <234g1a0527@srit.ac.in>
2026-01-10 05:53:29 +0000

    still image is not updated

--- next.config.ts
+++ next.config.ts
@@ -40,6 +40,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'images.static-collegedunia.com',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 };
