dbd7d36524a5e1a7e8df38de65bc5c4dfb4fc0b8
CHANDANA G <234g1a0527@srit.ac.in>
2026-01-10 05:35:37 +0000

    add gape between hero section and header part

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -66,7 +66,7 @@ export default function Header() {
 
 
   return (
-    <header className="sticky top-0 z-50 w-full bg-primary text-primary-foreground">
+    <header className="z-50 w-full bg-primary text-primary-foreground mb-8">
       <div className="container flex h-16 max-w-screen-2xl items-center">
         <div className="mr-4 flex">
           <Link href="/" className="mr-6 flex items-center space-x-2">
